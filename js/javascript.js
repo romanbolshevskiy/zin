03266f31be139b100c4a1ce5725c33e27ed9ebf3
рома большик < >
2016-07-21 22:18:44 +0300

    1st commit

---
+++ js/javascript.js
@@ -0,0 +1,31 @@
+﻿  $(document).ready(function() {
+   $("#tabs .bl img").click(function(){
+	   $(".popup").show(100);
+	   });
+	 $(".popup button").click(function(){
+		 $(".popup").hide(100);
+	  	
+		});
+
+$(".bl").click(function(){
+ var el= $(this).find('.name').text();
+ var el1= $(this).find('.sec').text();
+ var el2= $(this).find('.price1').text(); 
+ 
+ $('#box_1').fadeIn(300);
+ var a=$('#box_1 .blok  span.vala').text(el);
+ var a1=$('#box_1 .blok  span.second').text(el1);
+ var a2=$('#box_1 .blok  p.price').text(el2);
+ $('.zina').html('<img src="img/b.png"><b style="color:red">' +el2 + '</b>');
+ return false;
+});
+ 
+$('#tabs, .exit ').click(function(){
+ $('#box_1').fadeOut(300);
+ });
+ 
+   $('.hart').click(function(){
+ $('.zina').html('<img src="img/b.png">' + '0.00 Є*');
+ });
+		
+	});
